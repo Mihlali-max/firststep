783108710c216fe8f7c2f8778b43e8ba5bd25684
Mihlali Momoza <mihlali@firststep.co.za>
2026-05-15 04:56:04 -0400

    feat: app loading screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const loader = document.getElementById('app-loader'); if(loader) loader.style.display='none';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
